8b50af7dfd43339f3267d53790296323b7992c45
Takayuki-Suda <takayuki.suda.job@gmail.com>
2025-03-04 15:12:40 +0900

    Refactor: 'generate_url_for_keyword.py' と 'mercari_min_price_scraper.py' の並列実行を追加
    - 'generate_url_for_keyword.py' と 'mercari_min_price_scraper.py' を ThreadPoolExecutor を使って並列実行するように変更。
    - 'combined_min_price_scraper.py' と 'ebay.py' は引き続き順番に実行。

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
-# subprocess.run(["python", "generate_url_for_keyword.py"])
-# subprocess.run(["python", "mercari_min_price_scraper.py"])
-# subprocess.run(["python", "combined_min_price_scraper.py"])
+def run_script(script_name):
+    subprocess.run(["python", script_name])
+
+scripts = [
+    "generate_url_for_keyword.py",
+    "mercari_min_price_scraper.py",
+]
+
+with ThreadPoolExecutor() as executor:
+    executor.map(run_script, scripts)
+
+
+
+subprocess.run(["python", "combined_min_price_scraper.py"])
 subprocess.run(["python", "ebay.py"])
