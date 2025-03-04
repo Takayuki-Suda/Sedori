be7d39962ba3fa2c4eefed81e3b41f8e5efc1132
Takayuki-Suda <takayuki.suda.job@gmail.com>
2025-03-04 13:26:38 +0900

    Refactor: SeleniumからRequestsへ変更し、eBayの価格取得を高速化

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import subprocess
 
-subprocess.run(["python", "generate_url_for_keyword.py"])
-subprocess.run(["python", "mercari_min_price_scraper.py"])
-subprocess.run(["python", "combined_min_price_scraper.py"])
+# subprocess.run(["python", "generate_url_for_keyword.py"])
+# subprocess.run(["python", "mercari_min_price_scraper.py"])
+# subprocess.run(["python", "combined_min_price_scraper.py"])
+subprocess.run(["python", "ebay.py"])
